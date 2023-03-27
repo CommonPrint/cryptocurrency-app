6fe77671f786450b9476e581675ce1e2ca962921
Adlet Abdiyev <pioneermove@gmail.com>
2023-03-27 17:28:02 +0600

    feat: response data on endpoint ':/crypto' has been changed

--- server/index.js
+++ server/index.js
@@ -42,6 +42,42 @@ app.get('/cryptocurrency-list/:crypto', (req, res) => {
   // URL2 BRINGS BACK THE CURRENT QUOTE AND PRICE DATA
   const url2 = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=${crypto}`
 
+  let modifyData = {
+    metadata: {},
+    statistics: {}
+  };
+
+  request.get({
+    url: url,
+    json: true,
+    headers: {
+      'X-CMC_PRO_API_KEY': 'ef001fd1-93c5-4ad7-bc85-06a8bc183722'
+    }
+  }, (error, response, data) => {
+
+    if (error) {
+      return res.send({
+        error: error
+      });
+    }
+    
+    // res.send({
+    //   data: data
+    // });
+
+    
+    modifyData.metadata.id = data.data[`${crypto.toUpperCase()}`].id;
+    modifyData.metadata.name = data.data[`${crypto.toUpperCase()}`].name;
+    modifyData.metadata.symbol = data.data[`${crypto.toUpperCase()}`].symbol;
+    modifyData.metadata.description = data.data[`${crypto.toUpperCase()}`].description;
+    modifyData.metadata.urls = data.data[`${crypto.toUpperCase()}`].urls;
+    modifyData.metadata.logo = data.data[`${crypto.toUpperCase()}`].logo;
+    modifyData.metadata.subreddit = data.data[`${crypto.toUpperCase()}`].subreddit;
+    
+  });
+
+
+
   request.get({
     url: url2,
     json: true,
@@ -56,8 +92,12 @@ app.get('/cryptocurrency-list/:crypto', (req, res) => {
       });
     }
 
+    
+
+    modifyData.statistics = data.data[`${crypto.toUpperCase()}`].quote;
+
     res.send({
-      data: data
+      data: modifyData
     });
 
   });
