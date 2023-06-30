d8c222748a0b540d209632a2c155c6ec5f68e1cb
Adlet Abdiyev <127708590+CommonPrint@users.noreply.github.com>
2023-06-30 15:54:30 +0600

    feat: upload missing files

---
+++ server/server.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const app = express();
+const request = require('request');
+
+const icons2 = require('crypto-icons');
+
+console.log('icons: ', icons2);
+
+// let alert = require('alert');
+
+
+
+app.get('/', (req, res) => {
+
+    // URL2 BRINGS BACK THE CURRENT QUOTE AND PRICE DATA
+    const url = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest`
+    
+    const parameters = { 'slug': 'bitcoin', 'convert': 'USD' }
+
+    console.log('url: ', url);
+
+    request.get({
+      url: url,
+      json: true,
+      headers: {
+        'X-CMC_PRO_API_KEY': 'ef001fd1-93c5-4ad7-bc85-06a8bc183722'
+      },
+      params: parameters
+    }, (error, response, data) => {
+  
+      if (error) {
+        return res.send({
+          error: error
+        });
+      }
+
+      // console.log('data symbol: ', data.data[1].symbol);
+  
+      res.send({
+        data: data
+      });
+      
+    });
+  
+});
+
+
+app.get('/:crypto', (req, res) => {
+  const crypto = req.params.crypto
+  // URL BRINGS BACK ALL INFO OF THE TOKEN 
+  const url = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?symbol=${crypto}`
+  // URL2 BRINGS BACK THE CURRENT QUOTE AND PRICE DATA
+  const url2 = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=${crypto}`
+
+  request.get({
+    url: url2,
+    json: true,
+    headers: {
+      'X-CMC_PRO_API_KEY': 'ef001fd1-93c5-4ad7-bc85-06a8bc183722'
+    }
+  }, (error, response, data) => {
+
+    if (error) {
+      return res.send({
+        error: error
+      });
+    }
+
+    res.send({
+      data: data
+    });
+
+  });
+
+});
+
+
+app.listen(4000, () => {
+  console.log('Server listening on port 4000');
+});
